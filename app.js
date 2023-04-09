bbfddb189fbc9474a2feef392500fa05bfa8223b
Pohlonenko <Pohlonenko@mail.ru>
2023-04-09 19:34:11 +0300

    matchers

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-function sortHeroesByHealth(arg = []) {
-  return arg.sort((a, b) => b.health - a.health);
-}
 
-// sortHeroesByHealth([
-//   {name: 'мечник', health: 10},
-//   {name: 'маг', health: 100},
-//   {name: 'лучник', health: 80},
-// ])
